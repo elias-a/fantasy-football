faec67a953cf73542b437c2645763b31022ac175
Elias Alstead <elias-a@eliass-air.myfiosgateway.com>
2020-12-06 15:14:32 -0500

    Plot summary statistics of team scoring

--- funcs.py
+++ funcs.py
@@ -1,3 +1,4 @@
+import numpy as np
 import matplotlib.pyplot as plt
 from helpers import scrape
 from bs4 import BeautifulSoup
@@ -9,6 +10,7 @@ def GetTeams():
   url = 'https://fantasy.nfl.com/league/' + LEAGUE_ID
   filename = 'data/teams.txt'
 
+  # Scrape fresh data, if desired
   if UPDATE:
     scrape(url, filename)
 
@@ -26,6 +28,31 @@ def GetTeams():
   f.close()
 
   return teams
+
+# Return dictionary of team IDs and manager names
+def GetManagers():
+  url = 'https://fantasy.nfl.com/league/' + LEAGUE_ID + '/owners'
+  filename = 'data/managers.txt'
+
+  # Scrape fresh data, if desired
+  if UPDATE:
+    scrape(url, filename)
+
+  f = open(filename, 'r')
+
+  txt = f.read()
+  html = BeautifulSoup(txt, 'html.parser')
+
+  data = html.find_all('a', { 'class': 'teamName' })
+
+  managers = {}
+  for d in data:
+    manager = d.parent.parent.nextSibling.text
+    managers[d['class'][1][-1]] = manager
+
+  f.close()
+
+  return managers
 
 # Get weekly scores for each team
 def GetScores():
@@ -89,3 +116,42 @@ def PlotTeamScores(all_scores, teams):
     plt.plot(range(1, 13), scores)
 
   plt.show()
+
+def PlotSummaryStats(all_scores, teams):
+  fig = plt.figure()
+
+  team_ids = []
+  average = []
+  std = []
+  maximum = []
+  minimum = []
+  for team in teams:
+    team_ids.append(team)
+    scores = [all_scores[week][team] for week in all_scores]
+    
+    average.append(np.mean(scores))
+    std.append(np.std(scores))
+    maximum.append(np.max(scores))
+    minimum.append(np.min(scores))
+  
+  ax1 = fig.add_subplot(2, 2, 1)
+  ax1.bar(team_ids, average)
+  ax1.title.set_text('Average Score')
+
+  ax2 = fig.add_subplot(2, 2, 2)
+  ax2.bar(team_ids, std)
+  ax2.title.set_text('Standard Deviation of Average Score')
+
+  ax3 = fig.add_subplot(2, 2, 3)
+  ax3.bar(team_ids, maximum)
+  ax3.title.set_text('Maximum Score')
+
+  ax4 = fig.add_subplot(2, 2, 4)
+  ax4.bar(team_ids, minimum)
+  ax4.title.set_text('Minimum Score')
+
+  plt.show()
+
+  std_average = np.std(average)
+  std_max = np.std(maximum)
+  std_min = np.std(minimum)
--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import funcs
 # Get dictionary of team IDs and names
 teams_dict = funcs.GetTeams()
 
+# Get dictionary of team IDs and manager names
+managers_dict = funcs.GetManagers()
+
 # Get weekly scores for each team
 scores = funcs.GetScores()
 
@@ -11,3 +14,6 @@ funcs.PlotWeeklyScores(scores, teams_dict)
 
 # Plot line plots of scores by team
 funcs.PlotTeamScores(scores, teams_dict)
+
+# Plot summary statistics 
+funcs.PlotSummaryStats(scores, managers_dict)
